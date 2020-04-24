1e0cc9a60ce9b0088f4e8a9c06dcc35a76ea7c6e
velissariou <it12143@uom.edu.gr>
2020-04-24 19:21:32 +0300

    Renamed folders

--- Velissariou_Petros_4/src/SuspectsGraph.java
+++
@@ -1,9 +0,0 @@
-import edu.uci.ics.jung.graph.*;
-import edu.uci.ics.jung.visualization.VisualizationImageServer;
-public class SuspectsGraph {
-
-	public SuspectsGraph() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
